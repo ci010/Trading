c2580a4d2d962feff5bf050cc1a9d2d20745ded5
ci010 <18211182378@163.com>
2015-07-26 18:48:07 +0800

    create client proxy

--- src/main/java/net/ci010/trading/TradingMod.java
+++ src/main/java/net/ci010/trading/TradingMod.java
@@ -4,7 +4,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.SidedProxy;
 
