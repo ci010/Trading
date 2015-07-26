06b628f6257a2b0d02d153dc769a2cd2ec8608c4
ci010 <18211182378@163.com>
2015-07-26 18:42:14 +0800

    create mod class and proxy

---
+++ src/main/java/net/ci010/trading/CommonProxy.java
@@ -0,0 +1,6 @@
+package net.ci010.trading;
+
+public class CommonProxy
+{
+
+}
---
+++ src/main/java/net/ci010/trading/TradingMod.java
@@ -0,0 +1,33 @@
+package net.ci010.trading;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.SidedProxy;
+
+@Mod(name = TradingMod.NAME, modid = TradingMod.MODID, version = TradingMod.VERSION)
+public class TradingMod
+{
+	public static final String MODID = "tranding";
+	public static final String NAME = "Trading";
+	public static final String VERSION = "1.0";
+
+	@SidedProxy()
+	public static CommonProxy proxy;
+
+	@Instance("trading")
+	public static TradingMod instance;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+	}
+
+}
