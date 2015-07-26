dc48bcaac0917a51d9256c58cb13f2d4913eeafd
ci010 <18211182378@163.com>
2015-07-26 18:51:35 +0800

    locate the location of commonproxy

--- src/main/java/net/ci010/trading/TradingMod.java
+++ src/main/java/net/ci010/trading/TradingMod.java
@@ -14,7 +14,7 @@ public class TradingMod
 	public static final String NAME = "Trading";
 	public static final String VERSION = "1.0";
 
-	@SidedProxy()
+	@SidedProxy(serverSide = "net.ci010.trading.CommonProxy")
 	public static CommonProxy proxy;
 
 	@Instance("trading")
