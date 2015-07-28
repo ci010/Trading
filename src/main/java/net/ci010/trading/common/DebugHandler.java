13500216848c124b5794a6f5953204da198ac202
ci010 <18211182378@163.com>
2015-07-28 22:19:25 +0800

    dubug hander. jump to create a null container gui.

---
+++ src/main/java/net/ci010/trading/common/DebugHandler.java
@@ -0,0 +1,19 @@
+package net.ci010.trading.common;
+
+import net.ci010.trading.TradingMod;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class DebugHandler
+{
+	@SubscribeEvent
+	public void onPlayerJump(LivingJumpEvent event)
+	{
+		if(event.entityLiving instanceof EntityPlayer)
+		{
+			((EntityPlayer)event.entityLiving).openGui(TradingMod.instance, 0, event.entityLiving.worldObj, 0, 0, 0);
+		}
+	}
+}
+
