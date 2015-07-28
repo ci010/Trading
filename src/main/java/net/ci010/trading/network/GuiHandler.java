9d7d5b0cba7ee6223b62207598e5d4506106b6fd
ci010 <18211182378@163.com>
2015-07-28 22:17:17 +0800

    create GuiHandler to handle both client and server side. need testing if it's worked or not

---
+++ src/main/java/net/ci010/trading/network/GuiHandler.java
@@ -0,0 +1,38 @@
+package net.ci010.trading.network;
+
+import net.ci010.trading.client.gui.inventory.GuiTrade;
+import net.ci010.trading.common.TradingSystem;
+import net.ci010.trading.common.inventory.ContainerTrade;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
+
+public class GuiHandler implements IGuiHandler
+{
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+	{
+		switch (ID)
+		{
+			case 0:
+				return new ContainerTrade(player.inventory, TradingSystem.getPlayerTrade(player));
+			case 1:
+
+		}
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+	{
+		switch (ID)
+		{
+			case 0:
+				return new GuiTrade(new ContainerTrade(player.inventory, TradingSystem.getPlayerTrade(player)));
+			case 1:
+
+		}
+		return null;
+	}
+
+}
