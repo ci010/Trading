cd08a4f0d524b48ddd4a7b811c6983081cd5af97
ci010 <18211182378@163.com>
2015-07-28 22:10:42 +0800

    make proxy works. register new command.

--- src/main/java/net/ci010/trading/TradingMod.java
+++ src/main/java/net/ci010/trading/TradingMod.java
@@ -1,10 +1,14 @@
 package net.ci010.trading;
 
+import net.ci010.trading.common.CommonProxy;
+import net.ci010.trading.common.command.TradingCommand;
+import net.minecraft.command.ServerCommandManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.SidedProxy;
 
 @Mod(name = TradingMod.NAME, modid = TradingMod.MODID, version = TradingMod.VERSION)
@@ -14,7 +18,7 @@ public class TradingMod
 	public static final String NAME = "Trading";
 	public static final String VERSION = "1.0";
 
-	@SidedProxy(serverSide = "net.ci010.trading.CommonProxy")
+	@SidedProxy(serverSide = "net.ci010.trading.common.CommonProxy", clientSide = "net.ci010.trading.client.ClientProxy")
 	public static CommonProxy proxy;
 
 	@Instance("trading")
@@ -23,12 +27,20 @@ public class TradingMod
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
+		instance = this;
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
+		proxy.registerPackets();
+		proxy.registerHandler();
 	}
 	
-
+	@EventHandler
+	public void serverStarting(FMLServerStartingEvent event)
+	{
+		ServerCommandManager serverCommandManager = (ServerCommandManager) event.getServer().getCommandManager();
+		serverCommandManager.registerCommand(new TradingCommand());
+	}
 }
